e9c307606752e578d7a1fb1932e255209048e1e0
Savio Mendes <savio.souza@asaas.com.br>
2024-04-30 13:49:59 -0300

    Add: adicionando campos de mensagem de erros no formulário

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -5,9 +5,14 @@ import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("pedido")
@@ -16,12 +21,15 @@ public class PedidoController {
     private PedidoRepository pedidoRepository;
 
     @GetMapping("formulario")
-    public String formulario() {
+    public String formulario(RequisicaoNovoPedido requisicaoNovoPedido) {
         return "pedido/formulario";
     }
 
     @PostMapping("novo")
-    public String novo(RequisicaoNovoPedido requisicaoNovoPedido) {
+    public String novo(@Valid RequisicaoNovoPedido requisicaoNovoPedido, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pedido/formulario";
+        }
         Pedido pedido = requisicaoNovoPedido.toPedido();
         pedidoRepository.save(pedido);
         return "pedido/formulario";
